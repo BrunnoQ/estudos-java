96fd1d5cae80cf51b6e56283dcdbe719c056c9fb
Brunno Silva <brunnoqsilva@gmail.com>
2020-11-26 23:43:56 -0300

    Validador de email com REGEX, 100% caseiro

--- src/main/java/br/edu/brunno/geral/regex/TesteRegex.java
+++ src/main/java/br/edu/brunno/geral/regex/TesteRegex.java
@@ -15,6 +15,7 @@ public class TesteRegex {
    public static void main(String[] args) {
         
         validarCPF("335.901.451-60");
+        validarEmail("brunno.silva@teste.com.br");
 
     }
 
@@ -34,16 +35,51 @@ public class TesteRegex {
             int start = matcher.start();
             int end = matcher.end();
 
-            System.out.printf("Resultado: %s \n;" +
-                               "Grupo: %s \n;" +
-                               "Inicio: %s \n;" +
-                               "Fim: %s", match, grupo, start, end);
+            System.out.printf("Resultado: %s \n" +
+                               "Grupo: %s \n" +
+                               "Inicio: %s \n" +
+                               "Fim: %s \n", match, grupo, start, end);
 
         }
         else{
             System.out.println("Não Encontrou.");
         }
 
+    }
+
+    private static void validarEmail(String email){
+
+        String emailRegex = "^([\\w-\\.]+)@[\\w-]+(\\.[A-Za-z]{3})(\\.[A-Za-z]{2})?$";
+
+        //Engine do JAva
+        Pattern pattern = Pattern.compile(emailRegex);
+
+        //Busca Resultado
+        Matcher matcher = pattern.matcher(email);
+
+        //Resultado
+        if (matcher.find()) {
+            
+            String resultado = matcher.group();
+            String grupo1 = matcher.group(0);
+            String grupo2 = matcher.group(1);
+            String grupo3 = matcher.group(2);
+            String grupo4 = matcher.group(3);
+            int inicio = matcher.start();
+            int fim = matcher.end();
+
+            System.out.printf("Resultado: %s \n" +
+            "Grupo1: %s \n" +
+            "Grupo2: %s \n" +
+            "Grupo3: %s \n" +
+            "Grupo4: %s \n" +
+            "Inicio: %s \n" +
+            "Fim: %s \n", resultado, grupo1, grupo2, grupo3, grupo4, inicio,fim);
+
+        } else {
+            System.out.println("Email Inválido!");
+        }
+            
     }
     
 }
